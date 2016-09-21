03d5c9c0348156052acbe225f0b2b8b41ff22e56
Yosun Chang <yosun@nusoy.com>
2016-09-21 04:19:14 -0700

    thumb

--- crop.img.php
+++ crop.img.php
@@ -1,13 +1,18 @@
 <?php
- function makeThumb( $filepath, $thumbSize=300 ){
+ function makeThumb( $filepath, $thumbSize=200 ){
   // echo 'mt'.$filepath;
  /* Set Filenames */
   $srcFile = $filepath;//.'/'.$filename;//'blocks/img/gallery/'.$filename;
   $thumbFile = dirname($filepath) .'/thumb_'.basename($filepath);//.'.png';// $filepath.'/thumb/'.$filename;// 'blocks/img/gallery/thumbs/'.$filename;
 $thumbFilePath = $thumbFile;
 $im = imagecreatefrompng($filepath );
+$imgsize = getimagesize($im);
+    $width = $imgsize[0];
+    $height = $imgsize[1];
+imagecopyresampled()
 $to_crop_array = array('x' =>0 , 'y' => 0, 'width' => $thumbSize, 'height'=> $thumbSize);
-$thumb_im = imagecrop($im, $to_crop_array);
-imagepng($thumb_im, $thumbFilePath);
+$w_point = (($width - $width_new) / 2); 
+imagecopyresampled($dst_img, $src_img, 0, 0, $w_point, 0,   $thumbSize, $thumbSize,$width , $height);
+
   return $thumbFilePath;
 }
