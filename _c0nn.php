95e413201d52c968a334451b722fcb4fde4f4573
Yosun Chang <yosun@nusoy.com>
2016-09-20 22:40:55 -0700

    wha

--- _c0nn.php
+++ _c0nn.php
@@ -7,8 +7,9 @@ $db='snapglasses';
 //
 echo getenv("MYSQL_SERVICE_HOST");
 die('meh');
+/*
 $conn = mysql_connect( getenv("MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"),getenv("MYSQL_PASSWORD"));
 
 mysql_select_db($db,$conn);
 
-require_once('fn_mysql.php');
+require_once('fn_mysql.php');*/
