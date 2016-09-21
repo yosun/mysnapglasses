6de0409733636530d664098e053a23da74b083f7
Yosun Chang <yosun@nusoy.com>
2016-09-20 23:31:59 -0700

    test

--- _c0nn.php
+++ _c0nn.php
@@ -6,12 +6,9 @@ $u='userNX3';
 $p='iEOXLimq7i5axDUu';
 $db='snapglasses';
 
-//
-echo getenv("MYSQL_SERVICE_HOST");
-die('meh');
-/*
+ 
 $conn = mysql_connect( getenv("MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"),getenv("MYSQL_PASSWORD"));
 
 mysql_select_db($db,$conn);
 
-require_once('fn_mysql.php');*/
+require_once('fn_mysql.php');
