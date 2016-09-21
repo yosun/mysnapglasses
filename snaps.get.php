11c6878513dbc09dc79247a9e95fa7d2b08a27de
Yosun Chang <yosun@nusoy.com>
2016-09-21 01:42:52 -0700

    snaps.get.php

--- snaps.get.php
+++ snaps.get.php
@@ -1,1 +1,16 @@
-<?php require_once('_c0nn.php');
+<?php
+$username = $_REQUEST['username'];
+
+require_once('_c0nn.php'); $username=quoty($username);
+
+$query = 'SELECT * FROM snaps WHERE username='.$username;
+
+$result = mysql_query($query);
+
+while($row=mysql_fetch_assoc($result)){
+
+$json[]=array('id'=>$row['id'] , 'url'=> $row['url']);
+
+}
+
+echo json_encode($json);
--- snaps.post.php
+++ snaps.post.php
@@ -1,1 +1,1 @@
-<?php 
+<?php // deprecated to upload.php
